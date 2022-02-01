7066c88ba094a9543aee248c8d9de5df1882d9b8
Pedro <Pedro@a>
2022-02-01 11:55:46 +0100

    Haciendo la funcion para generar div

--- js/app.js
+++ js/app.js
@@ -1,1 +1,26 @@
 "use strict"
+
+//Seleccionamos el contenedor de la galeria
+const contenedor_galeria = document.querySelector("#contenedorGaleria");
+
+//Hacemos la funcion para crear los contenedores
+const crearContenedor=()=>{
+    //Contenedor de la foto y el texto
+    let nuevo_contenedor = document.createElement("div");
+
+    //Contenedor para la foto
+    let contenedor_foto = document.createElement("div");
+    contenedor_foto.classList.add("text-center");
+    contenedor_foto.classList.add("bordes-galeria");
+    contenedor_foto.classList.add("rounded-pill")
+    
+    nuevo_contenedor.appendChild(contenedor_foto);
+
+    //Contenedor para el texto
+    let contenedor_texto = document.createElement("div");
+    contenedor_texto.classList.add("text-center")
+
+    nuevo_contenedor.appendChild(contenedor_texto)
+
+    return nuevo_contenedor;
+} 
--- php/paginas/galeria.php
+++ php/paginas/galeria.php
@@ -5,6 +5,8 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Rubik:wght@300&display=swap" rel="stylesheet">
@@ -46,17 +48,12 @@
     <main>
         <div id="contenedor">
             <div class="insertar_datos">
-                <h2>¡FOTOS DE NUESTROS CLIENTES!</h2>
+                <h2>¡ ÚLTIMAS FOTOS DE NUESTROS CLIENTES !</h2>
             </div>
         </div>
-        <div id="contenedorSer">
-            <table id="tablas_mostrar">
-                <tr id="cabecera_tabla">
-                    <td>Nombre del servicio</td>
-                    <td>Duración</td>
-                    <td>Precio</td>
-                </tr>
-            </table>
+        <div  id="contenedorGaleria">
+            <div class="bordes-galeria rounded-pill">
+            </div>
         </div>
     </main>
 </body>
